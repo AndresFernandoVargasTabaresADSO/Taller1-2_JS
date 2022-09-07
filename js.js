77f9a44817a583cb253930b25028f31ba2cbe921
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-07 10:05:16 -0500

    Ejercicio 2

--- js.js
+++ js.js
@@ -1,8 +1,9 @@
-window.addEventListener("DOMContentLoaded",()=>{
-    // 1. Escribir un programa que sume, reste, multiplique y divida dos números
-    let num = Number(prompt("Escribe un numero", 2))
-    let num2 = Number(prompt("Escribe un segundo numero", 2))
+addEventListener("DOMContentLoaded",()=>{
 
-    console.log(`1. Escribir un programa que sume, reste, multiplique y divida dos números`)
-    console.log(`El resultado: ${num} + ${num2} es ${num + num2}`)
+    let lado = Number(prompt(`Escribe el lado de tu rectangulo: `,2))
+    let lado2 = Number(prompt(`Escribe el segundo lado de tu rectangulo: `,2))
+
+    
+    console.log(`2. Escribir un programa que calcule el área de un rectángulo: lado1 = 3 lado2 = 4 área del rectángulo= lado1 * lado2`)
+    console.log(`El area de tu rectangulo es: ${lado * lado2}`)
 })
