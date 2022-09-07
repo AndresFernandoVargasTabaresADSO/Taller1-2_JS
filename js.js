91592d48fc08c0843b1c58747371cc78b108f4f1
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-07 10:29:22 -0500

    Ejercicio 5

--- js.js
+++ js.js
@@ -1,9 +1,10 @@
 addEventListener("DOMContentLoaded",()=>{
-    console.log(`4. Escribir un programa que calcule el área de un triángulo:    Base = 7 altura = 4 área del triángulo = (base * altura)/2`)
+    console.log(`5. Escribir un programa que calcule la longitud y el área de una circunferencia: Radio = 4  Longitud de la circunferencia = 2 * PI * radio Área de la circunferencia = PI * radio˄2`)
 
-    let lado1 = Number(prompt(`Escribe la base de tu triangulo: `))
-    let lado2 = Number(prompt(`Ingresa la altura de tu triangulo: `))
-    let solucion= (lado1 * lado2)/2
+    let radio = Number(prompt(`Escribe el radio de tu circuferencia: `))
+    let longitud= (2 * Math.PI * radio)
+    let area= (Math.PI * radio * radio)
     
-    console.log(`El area de tu triangulo es: ${solucion}`)
+    console.log(`La longitug de tu circuferencia es: ${longitud}`)
+    console.log(`El area de tu circuferencia es: ${area}`)
 })
