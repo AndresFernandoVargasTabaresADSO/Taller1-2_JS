66fa22f825e1c761ee39a3700ca05f093df8193f
AndresFernandoVargasTabaresADSO <anonymatusrs@gmail.com>
2022-09-07 11:57:47 -0500

    Ejercicio 12

--- js.js
+++ js.js
@@ -1,12 +1,13 @@
 addEventListener("DOMContentLoaded",()=>{
-    console.log(`11. Escribir un programa que calcule el volumen de un elipsoide
-    V = (4/3) * PI * a * b *c`)
+    console.log(`12. Programa que muestre el pago de una llamada telefónica sabiendo que cada minuto cuesta $355 
+    pesos y un IVA de 20%.`)
 
-    let numa= Number(prompt(`Escribe la altura del elipsoide:  `))
-    let numb= Number(prompt(`Escribe el largo del elipsoide: `))
-    let numc= Number(prompt(`Escribe el ancho del elipsoide: `))
+    let temp= Number(prompt(`Ingrese los minutos que duro la llamada: `))
 
-    let solucion= ((4/3)* Math.PI * numa * numb * numc)
-
-    console.log(`El volumen del elipsoide es: ${solucion}`)
+    let precio= 355
+    let iva= (precio*0.20)
+    let solucion= (precio * temp)+iva
+    let solucion2= (precio * temp)
+    console.log(`Total sin iva: ${solucion2}`)
+    console.log(`Total mas 20% de iva ${solucion}`)
 })
